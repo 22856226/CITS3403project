a637490b5237ef4fee0bbd02531739c587c9d56a
Yongyi Lin <88419672+Regulus-lyy@users.noreply.github.com>
2022-05-12 23:17:27 +0800

    Add files via upload
    Program tests, need to modify later.

---
+++ app/tests.py
@@ -0,0 +1,16 @@
+import unittest
+from app import app, db, User
+
+class TestCase(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_one(self):
+        rv = one()
+        self.assertEqual(rv, 'admin')
+
+if __name__ == '__main__':
+    unittest.main()
