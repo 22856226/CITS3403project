cadf1f9687e60aa1071572b6863a8cf83774d04f
Yongyi Lin <88419672+Regulus-lyy@users.noreply.github.com>
2022-05-14 16:06:17 +0800

    Update __init__.py
    1. Update the path to locate the file to the project root directory.
    2. Creating a database

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,14 @@
 from flask import Flask
+from app.store_pwd import db
+import os
+import sys
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'you-will-never-guess'
+# Update the path to locate the file to the project root directory
+app.config['SECRET_KEY'] = 'dev'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////' + os.path.join(app.root_path, 'data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 from app import routes
+
+db.create_all()   # Creating a database
