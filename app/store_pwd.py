3bf9f707ed3e2c7face2416c5cad40af7441129a
Yongyi Lin <88419672+Regulus-lyy@users.noreply.github.com>
2022-05-17 16:43:48 +0800

    added some comments
    added some comments

--- app/store_pwd.py
+++ app/store_pwd.py
@@ -5,12 +5,12 @@ from flask import Flask
 app = Flask(__name__)
 db = SQLAlchemy(app)   #initialize the extension and pass in the app instance
 
-class User(db.Model):
+class User(db.Model):   #creating a database model
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20))
     password_hash = db.Column(db.String(128))
 
-    def set_password(self, password):
+    def set_password(self, password):   #check the password
         self.password_hash = generate_password_hash(password)
     def validate_password(self, password):
         return check_password_hash(self.password_hash, password)
