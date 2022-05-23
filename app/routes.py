d1870858b54cc2f4409b74b9fcc4eb1a5548ac8a
Yongyi Lin <22376411@student.uwa.edu.au>
2022-05-23 16:39:10 +0800

    Update
    Update

---
+++ app/routes.py
@@ -0,0 +1,79 @@
+from flask import render_template, flash, redirect,request,session
+from flask import Flask, url_for
+from app import app, db
+from app.forms import LoginForm
+from flask_login import current_user, login_user
+from app.models import User,Post
+from flask_login import logout_user
+from flask_login import login_required
+from werkzeug.urls import url_parse
+from app.forms import RegistrationForm
+
+@app.route('/')
+@app.route('/index',methods=['GET', 'POST'])
+@login_required
+def index():
+    if request.method == 'GET':
+        return render_template('sokoban.html')
+    else:
+        username= current_user.username
+        level1 = request.form.get("scores1")
+        level2 = request.form.get("scores2")
+        level3 = request.form.get("scores3")
+        id_num = current_user.id
+        data = Post.query.filter_by( user_name = username).first()
+        if data is None:
+            datas = Post(level1=level1, level2 = level2 ,level3 = level3,user_id = id_num, user_name = username)
+            db.session.add(datas)
+            db.session.commit()
+        else:
+            data.level1 = level1
+            data.level2 = level2
+            data.level3 = level3
+            db.session.commit()
+    return render_template('sokoban.html')
+@app.route('/play')
+def play():
+    return render_template('sokoban.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        flash('Login successfully!')
+        return redirect(next_page)
+    return render_template('login.html', title='Sign In', form=form)
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+@app.route('/user/<username>')
+
+@app.route('/ranking')
+def ranking():
+    content = db.session.query(Post).all()
+    return render_template('ranking.html',  content=content)
+
---
+++ tests.py
@@ -0,0 +1,69 @@
+import unittest
+from app import app, db
+from app.models import User
+from flask import Flask, session
+'''
+user = User(username='Tester', password_hash='123456789', scores='100')
+db.session.add(user)
+db.session.commit()
+
+user2 = User(username='Tester2', password_hash='987654321', scores='60')
+db.session.add(user2)
+db.session.commit()
+
+user.scores = user.scores + int(50)
+db.session.commit()
+print(user.scores)
+'''
+
+class GameTestCase(unittest.TestCase):
+    def setUp(self): # A new test client is created and a new database is initialized
+        app.config.update(
+            TESTING=True,
+            SQLALCHEMY_DATABASE_URI='sqlite:///:memory:'
+        )
+        db.create_all()
+        self.client = app.test_client()
+
+    # Test login, logout and register
+    def login(self, username, password):
+        return self.app.post('/login', data=dict(
+            username=username,
+            password=password
+            ), follow_redirects=True)
+    def logout(self):
+        return self.app.get('/signout', follow_redirects=True)
+    def register(self, username, email, password, password2):
+        return self.app.post('/register', data=dict(
+            username=username,
+            email=email,
+            password=password,
+            password2=password2
+            ),follow_redirects=True)
+
+    def test_login_logout(self):
+        rv = self.login('Tester', '123456789')   #enter username and password correctly
+        assert 'Login successfully!' in rv.data
+        rv = self.logout()   # test logout
+        assert 'You were logged out. Goodbye!' in rv.data
+        rv = self.login('wrong', '123456789')   #enter incorrect username but correct password
+        assert 'Incorrect username or password!' in rv.data
+        rv = self.login('Tester', '1234567')   #enter correct username but incorrect password
+        assert 'Incorrect username or password!' in rv.data
+
+    def test_register(self):
+        rv = self.register(username='Tester2', password='88888888')   # test register a account
+        assert 'Invalid input!' not in rv.data
+        assert 'Congratulations, you are now a registered player!' in rv.data
+        rv = self.register(username='', password='88888888')   # test blank input of username or password
+        assert 'Invalid input!' in rv.data
+        rv = self.register(username='Tester2', password='')
+        assert 'Invalid input!' in rv.data
+    
+    def tearDown(self):   #Close the file and delete it from the file system
+        db.session.remove()
+        db.drop_all()
+
+if __name__ == '__main__':
+    unittest.main()
+
